576b124d84a2475d34379d822ac359910cd30225
Nickolas Kenyeres <nickolas@knicklabs.com>
2019-05-20 22:18:12 -0400

    Refactor way pages are built

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,16 +5,20 @@ const api = require('./src/api')
 const DEFAULT_TYPE = 'page'
 const getPageTemplate = (type = DEFAULT_TYPE) => `src/templates/${type}Template.js`
 
-exports.createPages = ({ graphql, actions: { createPage } }) => Promise.all(
-  Object.keys(api).map(method => api[method](graphql))
-).then((...postSets) => postSets.forEach(
-  postSet => postSet.forEach(
-    posts => posts.filter(( { path }) => !!path).forEach(post => createPage({
+exports.createPages = ({ graphql, actions: { createPage } }) => 
+  Promise.all(Object.keys(api).map(method => api[method](graphql)))
+    .then((...postSets) => postSets.reduce((accumulator, postSet) => ([
+      ...accumulator,
+      ...postSet.reduce((accumulator, posts) => ([
+        ...accumulator,
+        ...posts,
+      ]), []),
+    ]), []))
+    .then(posts => posts.filter(({ path }) => !!path))
+    .then(posts => posts.forEach(post => createPage({
       path: post.path,
       component: path.resolve(getPageTemplate(post.type)),
       context: {
         post,
       }
-    }))
-  )
-))
+    })))
