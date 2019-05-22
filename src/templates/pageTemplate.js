7bcac0939f62f262f2b0737c49e7d09dedd78a44
Nickolas Kenyeres <nickolas@knicklabs.com>
2019-05-21 22:13:45 -0400

    Add example button

--- src/templates/pageTemplate.js
+++ src/templates/pageTemplate.js
@@ -2,11 +2,14 @@ import { Link } from 'gatsby'
 import Helmet from 'react-helmet'
 import React from 'react'
 
+import Button from '../components/Button'
+
 export default ({ pageContext: { post } } = { post: {} }) => (
   <div>
     <Helmet>
       <title>{post.title}</title>
     </Helmet>
+    <Button>Hello</Button>
     <h1>{post.title}</h1>
     <div dangerouslySetInnerHTML={{__html: post.content}} />
     {post.acf.posts.map(p => (
